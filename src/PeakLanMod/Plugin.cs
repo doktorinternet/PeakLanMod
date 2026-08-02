a26ea21838a9c8b1ac8d9ac99d82647830f72ffa
Anton Wahlberg <wahlberg.anton@gmail.com>
2026-08-02 18:44:45 +0200

    v0.2.0 UDP discovery

--- src/PeakLanMod/Plugin.cs
+++ src/PeakLanMod/Plugin.cs
@@ -50,7 +50,7 @@ public sealed class Plugin : BaseUnityPlugin
 
     public const string PluginGuid = "BadHorse.PeakLanMod";
     public const string PluginName = "PEAK LAN Mod";
-    public const string PluginVersion = "0.1.0";
+    public const string PluginVersion = "0.2.0";
 
     internal static ManualLogSource Log { get; private set; } = null!;
 
